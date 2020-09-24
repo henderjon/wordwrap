ad242715b38aefbf0895ec46d3cd80c0da1f8a75
henderjon <list@box370.net>
2020-09-24 13:27:18 -0500

    added some emojis

--- main.go
+++ main.go
@@ -18,99 +18,15 @@ var (
 	ColGuide = `---------------------------------------------------------|-----------|--|------|---------------------------------------|` // 58, 70, 72, 80, 120
 )
 
-func Wrap(s string, breakCol int) string {
-	var (
-		// n       int // counter
-		cc      int // char count
-		wordbuf bytes.Buffer
-		outbuf  strings.Builder
-		err     error
-		indent  []rune
-		isFirst = true
-		// ok      bool
-		// softLimit = breakCol - 8
-	)
-	in := bufio.NewScanner(strings.NewReader(s))
-	in.Split(bufio.ScanRunes)
-	for in.Scan() {
-
-		err = in.Err()
-		if !errors.Is(err, nil) {
-			log.Fatal(err)
-		}
-
-		rs := []rune(in.Text())
-		if len(rs) < 1 {
-			continue
-		}
-
-		if isFirst && unicode.IsSpace(rs[0]) {
-			// special case as one tab char is worth 4 space chars
-			if string(rs[0]) == HT {
-				rs = []rune("    ")
-			}
-			indent = append(indent, rs...)
-			continue
-		}
-
-		if isFirst {
-			outbuf.WriteString(string(indent))
-			cc = len(indent)
-			isFirst = false
-		}
-
-		// word count
-		wordbuf.WriteString(string(rs))
-
-		// soft break
-		// if cc > softLimit && unicode.IsSpace(rs[0]) {
-		// 	outbuf.WriteString(LF)
-		// 	cc = 0
-
-		// 	n, _ = outbuf.Write(wordbuf.Bytes())
-		// 	cc += n
-		// 	wordbuf.Reset()
-		// 	continue
-		// }
-
-		if unicode.IsSpace(rs[0]) {
-			wordLen := utf8.RuneCount(wordbuf.Bytes())
-			if cc+wordLen > breakCol {
-				outbuf.WriteString(LF)
-				outbuf.WriteString(string(indent))
-				cc = len(indent)
-			}
-
-			outbuf.Write(wordbuf.Bytes())
-			cc += wordLen
-			wordbuf.Reset()
-		}
-	}
-	// @TODO deal with last/trailing/ending words ...
-	if wordbuf.Len() > 0 {
-		wordLen := utf8.RuneCount(wordbuf.Bytes())
-		if cc+wordLen > breakCol {
-			outbuf.WriteString(LF)
-			outbuf.WriteString(string(indent))
-			cc = len(indent)
-		}
-
-		outbuf.Write(wordbuf.Bytes())
-	}
-
-	return outbuf.String()
-}
-
 func Simple(s string, breakCol int) string {
 	var (
-		// n       int // counter
-		cc      int // char count
-		wordbuf bytes.Buffer
-		outbuf  strings.Builder
-		err     error
-		indent  []rune
-		isFirst = true
-		// ok      bool
+		cc        int // char count
+		wordbuf   bytes.Buffer
+		outbuf    strings.Builder
+		err       error
+		indent    []rune
+		indentLen int
+		isFirst   = true
 		softLimit = breakCol - 8
 	)
 	in := bufio.NewScanner(strings.NewReader(s))
@@ -128,17 +44,21 @@ func Simple(s string, breakCol int) string {
 		}
 
 		if isFirst && unicode.IsSpace(rs[0]) {
+			indent = append(indent, rs...)
 			// special case as one tab char is worth 4 space chars
 			if string(rs[0]) == HT {
-				rs = []rune("    ")
+				cc += 4
+				indentLen += 4
+			} else {
+				indentLen += len(indent)
 			}
-			indent = append(indent, rs...)
+
 			continue
 		}
 
 		if isFirst {
 			outbuf.WriteString(string(indent))
-			cc = len(indent)
+			cc = indentLen
 			isFirst = false
 		}
 
@@ -150,7 +70,7 @@ func Simple(s string, breakCol int) string {
 			wordLen := utf8.RuneCount(wordbuf.Bytes())
 			outbuf.WriteString(LF)
 			outbuf.WriteString(string(indent))
-			cc = len(indent)
+			cc = indentLen
 
 			outbuf.Write(wordbuf.Bytes())
 			cc += wordLen
@@ -163,7 +83,7 @@ func Simple(s string, breakCol int) string {
 			if cc+wordLen > breakCol {
 				outbuf.WriteString(LF)
 				outbuf.WriteString(string(indent))
-				cc = len(indent)
+				cc = indentLen
 			}
 
 			outbuf.Write(wordbuf.Bytes())
@@ -177,95 +97,7 @@ func Simple(s string, breakCol int) string {
 		if cc+wordLen > breakCol {
 			outbuf.WriteString(LF)
 			outbuf.WriteString(string(indent))
-			cc = len(indent)
-		}
-
-		outbuf.Write(wordbuf.Bytes())
-	}
-
-	return outbuf.String()
-}
-
-func ReWrap(s string, breakCol int) string {
-	var (
-		// n       int // counter
-		cc      int // char count
-		wordbuf bytes.Buffer
-		outbuf  strings.Builder
-		err     error
-		indent  []rune
-		isFirst = true
-		// ok      bool
-		// softLimit = breakCol - 8
-	)
-	in := bufio.NewScanner(strings.NewReader(s))
-	in.Split(bufio.ScanRunes)
-	for in.Scan() {
-
-		err = in.Err()
-		if !errors.Is(err, nil) {
-			log.Fatal(err)
-		}
-
-		rs := []rune(in.Text())
-		if len(rs) < 1 {
-			continue
-		}
-
-		if string(rs[0]) == LF {
-			rs = []rune(SPACE)
-			isFirst = false
-		}
-
-		if isFirst && unicode.IsSpace(rs[0]) {
-			// special case as one tab char is worth 4 space chars
-			if string(rs[0]) == HT {
-				rs = []rune("    ")
-			}
-			indent = append(indent, rs...)
-			continue
-		}
-
-		if isFirst {
-			outbuf.WriteString(string(indent))
-			cc = len(indent)
-			isFirst = false
-		}
-
-		// word count
-		wordbuf.WriteString(string(rs))
-
-		// soft break
-		// if cc > softLimit && unicode.IsSpace(rs[0]) {
-		// 	outbuf.WriteString(LF)
-		// 	cc = 0
-
-		// 	n, _ = outbuf.Write(wordbuf.Bytes())
-		// 	cc += n
-		// 	wordbuf.Reset()
-		// 	continue
-		// }
-
-		if unicode.IsSpace(rs[0]) {
-			wordLen := utf8.RuneCount(wordbuf.Bytes())
-			if cc+wordLen > breakCol {
-				outbuf.WriteString(LF)
-				outbuf.WriteString(string(indent))
-				cc = len(indent)
-			}
-
-			outbuf.Write(wordbuf.Bytes())
-			cc += wordLen
-			wordbuf.Reset()
-		}
-	}
-	// @TODO deal with last/trailing/ending words ...
-	if wordbuf.Len() > 0 {
-		wordLen := utf8.RuneCount(wordbuf.Bytes())
-		if cc+wordLen > breakCol {
-			outbuf.WriteString(LF)
-			outbuf.WriteString(string(indent))
-			cc = len(indent)
+			cc = indentLen
 		}
 
 		outbuf.Write(wordbuf.Bytes())
